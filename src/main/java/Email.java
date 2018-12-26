cd2d415736533d6ca607c0430089a54728f66f40
shayyakubov <shayyakubov@gmail.com>
2018-12-26 19:38:19 +0200

    initial commit

---
+++ src/main/java/Email.java
@@ -0,0 +1,38 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Email {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String alternateEmail;
+
+    //constructor to receive the first and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        Logger logger = Logger.getLogger("emailAppLogger");
+        logger.log(Level.FINE, "Created new Email for " + firstName + " " + lastName);
+    }
+    //Ask or set the department
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    //Generate random password
+
+    //set mailBox capacity
+
+    //set an alternate email
+
+    //change the password
+
+}
---
+++ src/main/java/EmailApp.java
@@ -0,0 +1,8 @@
+public class EmailApp {
+
+    public static void main(String [] args){
+        Email shaysMail = new Email("shay","Yaakobov");
+
+
+    }
+}
